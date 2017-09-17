6bb7ad4978555dad388c5aaba4ac8c8c3ceeecd1
delyanastoyanova <delyanastoyanova96@gmail.com>
2017-09-17 22:28:39 +0200

    Exceptions for wrong user input

---
+++ src/ComputationTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ComputationTest {
+    @Test
+    public void compute() throws Exception {
+    }
+
+}
---
+++ src/NumberException.java
@@ -0,0 +1,5 @@
+public class NumberException extends Exception {
+    NumberException(String message) {
+        super(message);
+    }
+}
